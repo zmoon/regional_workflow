a733320b8c28288432a4bb76b61b05195cc748a0
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2022-06-06 13:40:04 +0000

    port to wcoss2

---
+++ ush/machine/wcoss2.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+function file_location() {
+
+  # Return the default location of external model files on disk
+
+  local external_file_fmt external_model location
+
+  external_model=${1}
+  external_file_fmt=${2}
+
+  location=""
+  case ${external_model} in
+
+    "FV3GFS")
+      location='/lfs/h1/ops/prod/com/gfs/v16.2/gfs.${YYYYMMDD}/${HH}/atmos'
+      ;;
+    "RAP")
+      location='/lfs/h1/ops/prod/com/rap/v5.1'
+      ;;
+    "HRRR")
+      location='/lfs/h1/ops/prod/com/hrrr/v4.1'
+      ;;
+    "NAM")
+      location='/lfs/h1/ops/prod/com/nam/v4.2'
+      ;;
+
+  esac
+  echo ${location:-}
+
+}
+
+EXTRN_MDL_SYSBASEDIR_ICS=${EXTRN_MDL_SYSBASEDIR_ICS:-$(file_location \
+  ${EXTRN_MDL_NAME_ICS} \
+  ${FV3GFS_FILE_FMT_ICS})}
+EXTRN_MDL_SYSBASEDIR_LBCS=${EXTRN_MDL_SYSBASEDIR_LBCS:-$(file_location \
+  ${EXTRN_MDL_NAME_LBCS} \
+  ${FV3GFS_FILE_FMT_LBCS})}
+
+EXTRN_MDL_DATA_STORES=${EXTRN_MDL_DATA_STORES:-"hpss"}
+
+# System scripts to source to initialize various commands within workflow
+# scripts (e.g. "module").
+if [ -z ${ENV_INIT_SCRIPTS_FPS:-""} ]; then
+  ENV_INIT_SCRIPTS_FPS=( "/etc/profile" "/usrx/local/prod/lmod/lmod/init/sh" )
+fi
+
+# Commands to run at the start of each workflow task.
+PRE_TASK_CMDS='{ ulimit -s unlimited; ulimit -a; }'
+
+# Architecture information
+WORKFLOW_MANAGER="rocoto"
+NCORES_PER_NODE=${NCORES_PER_NODE:-24}
+SCHED=${SCHED:-"lsf"}
+QUEUE_DEFAULT=${QUEUE_DEFAULT:-"dev"}
+QUEUE_HPSS=${QUEUE_HPSS:-"dev_transfer"}
+QUEUE_FCST=${QUEUE_FCST:-"dev"}
+
+# UFS SRW App specific paths
+staged_data_dir="/lfs/h2/emc/lam/noscrub/UFS_SRW_App/develop"
+FIXgsm=${FIXgsm:-"${staged_data_dir}/fix/fix_am"}
+FIXaer=${FIXaer:-"${staged_data_dir}/fix/fix_aer"}
+FIXlut=${FIXlut:-"${staged_data_dir}/fix/fix_lut"}
+TOPO_DIR=${TOPO_DIR:-"${staged_data_dir}/fix/fix_orog"}
+SFC_CLIMO_INPUT_DIR=${SFC_CLIMO_INPUT_DIR:-"${staged_data_dir}/fix/fix_sfc_climo"}
+DOMAIN_PREGEN_BASEDIR=${DOMAIN_PREGEN_BASEDIR:-"${staged_data_dir}/FV3LAM_pregen"}
+
+# Run commands for executables
+RUN_CMD_SERIAL="mpiexec"
+RUN_CMD_UTILS="mpiexec"
+RUN_CMD_FCST='mpiexec -n ${PE_MEMBER01}'
+RUN_CMD_POST="mpiexec"
+
+# MET/METplus-Related Paths
+MET_INSTALL_DIR=${MET_INSTALL_DIR:-"/apps/ops/para/libs/intel/19.1.3.304/met/10.0.1"}
+METPLUS_PATH=${METPLUS_PATH:-"/apps/ops/para/libs/intel/19.1.3.304/metplus/4.0.0"}
+CCPA_OBS_DIR=${CCPA_OBS_DIR:-"${staged_data_dir}/obs_data/ccpa/proc"}
+MRMS_OBS_DIR=${MRMS_OBS_DIR:-"${staged_data_dir}/obs_data/mrms/proc"}
+NDAS_OBS_DIR=${NDAS_OBS_DIR:-"${staged_data_dir}/obs_data/ndas/proc"}
+MET_BIN_EXEC=${MET_BIN_EXEC:-"exec"}
+
+# Test Data Locations
+TEST_PREGEN_BASEDIR="${staged_data_dir}/FV3LAM_pregen"
+TEST_COMIN="${staged_data_dir}/COMGFS"
+TEST_EXTRN_MDL_SOURCE_BASEDIR="${staged_data_dir}/input_model_data"
